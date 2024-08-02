ee8e30e35fa48feffa201244bdb69d925affc6a3
zxl hhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2024-08-02 19:56:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -19,7 +19,8 @@ https://sub.xf.free.hr/auto
 `
 
 let urls = [];
-let subconverter = "subapi-loadbalancing.pages.dev"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+//let subconverter = "subapi-loadbalancing.pages.dev"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subconverter = "url.v1.mk"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psu
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
 
